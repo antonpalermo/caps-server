3a26fa56a3ddce2c3c83079e26edad827019ff28
Anton Palermo <ANPBusinessmail@gmail.com>
2022-02-17 19:53:46 +0800

    refac: move auth/login to user/authenticate

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common'
 import { PassportModule } from '@nestjs/passport'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { User } from 'src/user/entities/user.entity'
-import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { LocalStrategy } from './local.strategy'
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), PassportModule],
-  controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
   exports: [AuthService]
 })
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,25 +1,37 @@
-import { Body, Controller, Get, Patch, Post, Query } from '@nestjs/common'
-import { AuthService } from '../auth/auth.service'
+import {
+  Body,
+  Controller,
+  Get,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+  Request
+} from '@nestjs/common'
+import { Request as ExpressRequest } from 'express'
+import { LocalAuthGuard } from 'src/auth/local-auth.guard'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
 import { User } from './entities/user.entity'
 import { UserService } from './user.service'
 
+type Request = ExpressRequest & {
+  user: Partial<User>
+}
+
 @Controller({ path: 'user', version: '1' })
 export class UserController {
-  constructor(
-    private readonly userSrv: UserService,
-    private readonly authSrv: AuthService
-  ) {}
+  constructor(private readonly userSrv: UserService) {}
 
   @Get()
   async user(@Query('id') id: string): Promise<User | undefined> {
     return await this.userSrv.user(id)
   }
 
+  @UseGuards(LocalAuthGuard)
   @Post('authenticate')
-  async authenticate(@Body() data: any): Promise<any> {
-    return await this.authSrv.validate(data.identity, data.password)
+  async authenticate(@Request() req: Request): Promise<any> {
+    return req.user
   }
 
   @Post('create')
